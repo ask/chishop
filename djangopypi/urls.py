0c0683a5e40ca7215058c9d3064d048fd350d25f
Russell Sim <russell.sim@vpac.org>
2009-04-15 11:42:34 +1000

    cleaned up url patterns

--- djangopypi/urls.py
+++ djangopypi/urls.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, url, include
 
-urlpatterns = patterns("",
+urlpatterns = patterns("djangopypi.views",
     # Simple PyPI
-    url(r'^/?$', "djangopypi.views.simple",
+    url(r'^$', "simple",
         name="djangopypi-simple"),
 
-    url(r'^(?P<dist_name>[\w\d_\.\-]+)/(?P<version>[\w\.\d\-_]+)/?',
-        "djangopypi.views.show_version",
+    url(r'^(?P<dist_name>[\w\d_\.\-]+)/(?P<version>[\w\.\d\-_]+)/$',
+        "show_version",
         name="djangopypi-show_version"),
 
-    url(r'^(?P<dist_name>[\w\d_\.\-]+)/?', "djangopypi.views.show_links",
+    url(r'^(?P<dist_name>[\w\d_\.\-]+)/$', "show_links",
         name="djangopypi-show_links"),
 )
 
