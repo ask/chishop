b1f72a271712cf34bd2e0749e3dce0ef6cd3e0c9
Ask Solem <askh@opera.com>
2009-11-04 11:08:17 +0100

    Refactor the views

--- djangopypi/http.py
+++ djangopypi/http.py
@@ -1,37 +1,7 @@
-""" http.py
-
-Copyright (c) 2009, Ask Solem
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice,
-      this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-
-Neither the name of Ask Solem nor the names of its contributors may be used
-to endorse or promote products derived from this software without specific
-prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-"""
-
-
 from django.http import HttpResponse
+from django.utils.datastructures import MultiValueDict
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 
 class HttpResponseNotImplemented(HttpResponse):
     status_code = 501
@@ -45,4 +15,60 @@ class HttpResponseUnauthorized(HttpResponse):
         self['WWW-Authenticate'] = 'Basic realm="%s"' % realm
 
 
+def parse_weird_post_data(raw_post_data):
+    """For some reason Django can't parse the HTTP POST data
+    sent by ``distutils`` register/upload commands.
 
+    This parser should be able to so, and returns a
+    :class:`django.utils.datastructures.MultiValueDict`
+    as you would expect from a regular ``request.POST`` object.
+
+    """
+    # If anyone knows any better way to do this, you're welcome
+    # to give me a solid beating. [askh@opera.com]
+    sep = raw_post_data.splitlines()[1]
+    items = raw_post_data.split(sep)
+    post_data = {}
+    files = {}
+    for part in filter(lambda e: not e.isspace(), items):
+        item = part.splitlines()
+        if len(item) < 2:
+            continue
+        header = item[1].replace("Content-Disposition: form-data; ", "")
+        kvpairs = header.split(";")
+        headers = {}
+        for kvpair in kvpairs:
+            if not kvpair:
+                continue
+            key, value = kvpair.split("=")
+            headers[key] = value.strip('"')
+        if "name" not in headers:
+            continue
+        content = part[len("\n".join(item[0:2]))+2:len(part)-1]
+        if "filename" in headers:
+            file = SimpleUploadedFile(headers["filename"], content,
+                    content_type="application/gzip")
+            files["distribution"] = [file]
+        elif headers["name"] in post_data:
+            post_data[headers["name"]].append(content)
+        else:
+            # Distutils sends UNKNOWN for empty fields (e.g platform)
+            # [russell.sim@gmail.com]
+            if content == 'UNKNOWN':
+                post_data[headers["name"]] = [None]
+            else:
+                post_data[headers["name"]] = [content]
+
+    return MultiValueDict(post_data), MultiValueDict(files)
+
+
+def login_basic_auth(request):
+    authentication = request.META.get("HTTP_AUTHORIZATION")
+    if not authentication:
+        return
+    (authmeth, auth) = authentication.split(' ', 1)
+    if authmeth.lower() != "basic":
+        return
+    auth = auth.strip().decode("base64")
+    username, password = auth.split(":", 1)
+    return authenticate(username=username, password=password)
--- djangopypi/views.py
+++ djangopypi/views.py
@@ -1,197 +1,42 @@
-""" views.py
-
-Copyright (c) 2009, Ask Solem
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright notice,
-      this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-
-Neither the name of Ask Solem nor the names of its contributors may be used
-to endorse or promote products derived from this software without specific
-prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-"""
-
 import os
-
-from django.conf import settings
-from django.http import Http404, HttpResponse, HttpResponseBadRequest
-from django.http import QueryDict, HttpResponseForbidden
-from django.shortcuts import render_to_response
-from djangopypi.models import Project, Classifier, Release, UPLOAD_TO
-from djangopypi.forms import ProjectForm, ReleaseForm
+from django.http import Http404, HttpResponseBadRequest
 from django.template import RequestContext
-from django.utils.datastructures import MultiValueDict
+from django.shortcuts import render_to_response
 from django.utils.translation import ugettext_lazy as _
-from django.core.files.uploadedfile import SimpleUploadedFile
-from django.contrib.auth import authenticate, login
-from djangopypi.http import HttpResponseNotImplemented
-from djangopypi.http import HttpResponseUnauthorized
-
-
-ALREADY_EXISTS_FMT = _("""A file named "%s" already exists for %s. To fix """
-                     + "problems with that you should create a new release.")
-
-
-def parse_weird_post_data(raw_post_data):
-    """ For some reason Django can't parse the HTTP POST data
-    sent by ``distutils`` register/upload commands.
-
-    This parser should be able to so, and returns a
-    :class:`django.utils.datastructures.MultiValueDict`
-    as you would expect from a regular ``request.POST`` object.
-    """
-    # If anyone knows any better way to do this, you're welcome
-    # to give me a solid beating. [askh@opera.com]
-    sep = raw_post_data.splitlines()[1]
-    items = raw_post_data.split(sep)
-    post_data = {}
-    files = {}
-    for part in [e for e in items if not e.isspace()]:
-        item = part.splitlines()
-        if len(item) < 2: continue
-        header = item[1].replace("Content-Disposition: form-data; ", "")
-        kvpairs = header.split(";")
-        headers = {}
-        for kvpair in kvpairs:
-            if not kvpair: continue
-            key, value = kvpair.split("=")
-            headers[key] = value.strip('"')
-        if not "name" in headers: continue
-        content = part[len("\n".join(item[0:2]))+2:len(part)-1]
-        if "filename" in headers:
-            file = SimpleUploadedFile(headers["filename"], content,
-                    content_type="application/gzip")
-            files["distribution"] = [file]
-        elif headers["name"] in post_data:
-            post_data[headers["name"]].append(content)
-        else:
-            # Distutils sends UNKNOWN for empty fields (e.g platform)
-            # [russell.sim@gmail.com]
-            if content == 'UNKNOWN':
-                post_data[headers["name"]] = [None]
-            else:
-                post_data[headers["name"]] = [content]
-
-    return MultiValueDict(post_data), MultiValueDict(files)
-
-
-def login_basic_auth(request):
-    authentication = request.META.get("HTTP_AUTHORIZATION")
-    if not authentication:
-        return
-    (authmeth, auth) = authentication.split(' ', 1)
-    if authmeth.lower() != "basic":
-        return
-    auth = auth.strip().decode("base64")
-    username, password = auth.split(":", 1)
-    return authenticate(username=username, password=password)
-
-
-def submit_project_or_release(user, post_data, files):
-    """Registers/updates a project or release"""
-    try:
-        project = Project.objects.get(name=post_data['name'])
-        if project.owner != user:
-            return HttpResponseForbidden(
-                    "That project is owned by someone else!")
-    except Project.DoesNotExist:
-        project = None
-
-    project_form = ProjectForm(post_data, instance=project)
-    if project_form.is_valid():
-        project = project_form.save(commit=False)
-        project.owner = user
-        project.save()
-        for c in post_data.getlist('classifiers'):
-            classifier, created = Classifier.objects.get_or_create(name=c)
-            project.classifiers.add(classifier)
-        if files:
-            allow_overwrite = getattr(settings,
-                "DJANGOPYPI_ALLOW_VERSION_OVERWRITE", False)
-            try:
-                release = Release.objects.get(version=post_data['version'],
-                                              project=project,
-                                              distribution=UPLOAD_TO + '/' +
-                                              files['distribution']._name)
-                if not allow_overwrite:
-                    return HttpResponseForbidden(ALREADY_EXISTS_FMT % (
-                                release.filename, release))
-            except Release.DoesNotExist:
-                release = None
-
-            # If the old file already exists, django will append a _ after the
-            # filename, however with .tar.gz files django does the "wrong"
-            # thing and saves it as project-0.1.2.tar_.gz. So remove it before
-            # django sees anything.
-            release_form = ReleaseForm(post_data, files, instance=release)
-            if release_form.is_valid():
-                if release and os.path.exists(release.distribution.path):
-                    os.remove(release.distribution.path)
-                release = release_form.save(commit=False)
-                release.project = project
-                release.save()
-            else:
-                return HttpResponseBadRequest(
-                        "ERRORS: %s" % release_form.errors)
-    else:
-        return HttpResponseBadRequest("ERRORS: %s" % project_form.errors)
-
-    return HttpResponse()
-
-
-def register_or_upload(request, post_data, files):
-    user = login_basic_auth(request)
-    if not user:
-        return HttpResponseUnauthorized('pypi')
-
-    login(request, user)
-    if not request.user.is_authenticated():
-        return HttpResponseForbidden(
-                "Not logged in, or invalid username/password.")
-
-    return submit_project_or_release(user, post_data, files)
+from djangopypi.http import HttpResponseNotImplemented, parse_weird_post_data
+from djangopypi.models import Project
+from djangopypi import actions
 
 ACTIONS = {
-    # file_upload is the action used with distutils ``sdist`` command.
-    "file_upload": register_or_upload,
+    # file_upload is the action used with the distutils ``sdist`` command.
+    "file_upload": actions.register_or_upload,
 
-    # submit is the :action used with distutils ``register`` command.
-    "submit": register_or_upload,
+    # submit is the :action used with the distutils ``register`` command.
+    "submit": actions.register_or_upload,
 }
 
 
-def simple(request, template_name="djangopypi/simple.html"):
+def action(request):
+    if request.method != "POST":
+        return HttpResponseBadRequest()
+
+    post_data, files = parse_weird_post_data(request.raw_post_data)
+    action_name = post_data.get(":action")
+    if action_name not in ACTIONS:
+        return HttpResponseNotImplemented(
+            "Action %s is not implemented" % action_name)
+    return ACTIONS[action_name](request, post_data, files)
+
+
+def simple(request, template_name="djangopypi/simple.html",
+        title=_("Package Index")):
     if request.method == "POST":
-        post_data, files = parse_weird_post_data(request.raw_post_data)
-        action_name = post_data.get(":action")
-        if action_name not in ACTIONS:
-            return HttpResponseNotImplemented(
-                "The action %s is not implemented" % action_name)
-        return ACTIONS[action_name](request, post_data, files)
+        return action(request)
 
     dists = Project.objects.all().order_by("name")
     context = RequestContext(request, {
         "dists": dists,
-        "title": 'Package Index',
+        "title": title,
     })
 
     return render_to_response(template_name, context_instance=context)
@@ -203,7 +48,7 @@ def show_links(request, dist_name,
         project = Project.objects.get(name=dist_name)
         releases = project.releases.all().order_by('-version')
     except Project.DoesNotExist:
-        raise Http404
+        raise Http404(_("No such project: %s" % dist_name))
 
     context = RequestContext(request, {
         "dist_name": dist_name,
@@ -218,10 +63,10 @@ def show_links(request, dist_name,
 def show_version(request, dist_name, version,
         template_name="djangopypi/show_version.html"):
     try:
-        release = Project.objects.get(name=dist_name).releases \
-                                        .get(version=version)
+        project = Project.objects.get(name=dist_name)
+        release = project.releases.get(version=version)
     except Project.DoesNotExist:
-        raise Http404()
+        raise Http404(_("No such project: %s" % project))
 
     context = RequestContext(request, {
         "dist_name": dist_name,
